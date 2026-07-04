beacbbe684ff5ad37e453fa664ec022456ad1964
Jason Skillman <jason_skillman@outlook.com>
2026-07-04 09:44:28 -0400

    Cleanup

--- Runtime/Scripts/WindowsFilePicker.cs
+++ Runtime/Scripts/WindowsFilePicker.cs
@@ -1,25 +1,25 @@
-﻿namespace JasonSkillman.UnityWindowsFilePicker
-{
-	using System;
-	using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
+namespace JasonSkillman.UnityWindowsFilePicker
+{
 	public static class WindowsFilePicker
 	{
 		[DllImport("WindowsFilePickerPlugin", CharSet = CharSet.Unicode)]
 		private static extern bool OpenFilePickerNative([Out] char[] outPath, int maxLength, string initialDirectory, string filter);
-		
+
 		public static bool OpenFilePicker(out string path, string initialDirectory = null, string filter = null)
 		{
 			if(string.IsNullOrEmpty(initialDirectory))
 			{
 				initialDirectory = Environment.CurrentDirectory;
 			}
-			
+
 			if(string.IsNullOrEmpty(filter))
 			{
 				filter = "All Files\0*.*\0\0";
 			}
-			
+
 			char[] buffer = new char[1024];
 			if(OpenFilePickerNative(buffer, buffer.Length, initialDirectory, filter))
 			{
